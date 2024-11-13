69f79398ce1c3b2496e82e276d35aa220f4ccdb6
TweenzY-Y <tweenzy.mail@gmail.com>
2024-11-13 13:07:27 +0100

    Optimization + New map provider

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ function toggleMobileNav() {
 document.addEventListener("DOMContentLoaded", () => {
   hamburger.addEventListener("click", toggleMobileNav);
 });
+
+function loadMap() {
+  const mapPlaceholder = document.getElementById("map-placeholder");
+  mapPlaceholder.innerHTML = `
+      <iframe
+        src="https://maps.google.com/maps?&hl=en&q=Villa Del Arte, 34-100 Wadowice&t=&z=17&ie=UTF8&iwloc=B&output=embed"
+        width="100%"
+        height="400"
+        style="border: 0"
+        allowfullscreen=""
+        loading="lazy"
+        title="Mapa google maps z zaznaczoną lokalizacją naszego hotelu"
+      ></iframe>
+    `;
+}
